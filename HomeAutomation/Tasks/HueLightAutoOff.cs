058897c70879746e68328c96381b712375b86bfb
testakina <testakina@gmail.com>
2025-12-31 14:46:11 +0900

    PC起動時自動オン

--- HomeAutomation/Tasks/HueLightAutoOff.cs
+++ HomeAutomation/Tasks/HueLightAutoOff.cs
@@ -17,7 +17,8 @@ public class HueLightAutoOff : IAutomationTask
         this._logger.LogInformation($"Start");
         var ping = new Ping();
         var httpClient = new HttpClient();
-        var content = new StringContent(@"{""on"":false}");
+        var contentOn = new StringContent(@"{""on"":true}");
+        var contentOff = new StringContent(@"{""on"":false}");
         while (true)
         {
             this._logger.LogInformation($"Check");
@@ -29,10 +30,22 @@ public class HueLightAutoOff : IAutomationTask
                 reply = await ping.SendPingAsync("13700k.localnet");
                 if (reply.Status != IPStatus.Success)
                 {
-                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/1/state", content);
-                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/2/state", content);
-                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/3/state", content);
-                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/4/state", content);
+                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/1/state", contentOff);
+                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/2/state", contentOff);
+                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/3/state", contentOff);
+                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/4/state", contentOff);
+                }
+            }
+            else if (reply.Status == IPStatus.Success)
+            {
+                await Task.Delay(new TimeSpan(0, 0, 20));
+                reply = await ping.SendPingAsync("13700k.localnet");
+                if (reply.Status == IPStatus.Success)
+                {
+                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/1/state", contentOn);
+                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/2/state", contentOn);
+                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/3/state", contentOn);
+                    await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/4/state", contentOn);
                 }
             }
         }
